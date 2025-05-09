f9a72d8221252975932705155db4e2eaee5bb446
Rathna90 <s.rathnamca@gmail.com>
2025-05-09 20:34:48 +0530

    CRYPTOFERN

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB761UZH4dX5LalMhfinl9EpvW_VMAwOdY",
-    authDomain: "crypto-hunter-92155.firebaseapp.com",
-    projectId: "crypto-hunter-92155",
-    storageBucket: "crypto-hunter-92155.appspot.com",
-    messagingSenderId: "286942384437",
-    appId: "1:286942384437:web:a6586b4dfb04cc9b547c7c"
-  };
-
+  apiKey: "AIzaSyA5psgvIK6TYvucjxDtddKysoa7uBcDDWo",
+  authDomain: "project25-17c17.firebaseapp.com",
+  databaseURL: "https://project25-17c17-default-rtdb.firebaseio.com",
+  projectId: "project25-17c17",
+  storageBucket: "project25-17c17.firebasestorage.app",
+  messagingSenderId: "491869566557",
+  appId: "1:491869566557:web:f53830faa255be4d76ffb4",
+  measurementId: "G-2MNJYQNDLZ"
+};
 export default firebaseConfig;
 
 
